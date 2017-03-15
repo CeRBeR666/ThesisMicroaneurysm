1b36d3aa88770d5e4c6befdccdb7bb0c9966c5cc
CeRBeR <agaoglu.volkan@gmail.com>
2017-03-15 23:13:04 +0300

    İlk commit opencv eklenmemiş hali

---
+++ app/src/main/java/com/agaoglu/tez/hastaSec.java
@@ -0,0 +1,36 @@
+package com.agaoglu.tez;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class hastaSec extends Fragment {
+    public hastaSec() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_hasta_sec, container, false);
+
+        return view;
+    }
+}
