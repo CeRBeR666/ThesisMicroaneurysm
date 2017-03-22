678efdd9a4fdd8584f9ab405dede46f820e69f3b
CeRBeR <agaoglu.volkan@gmail.com>
2017-03-22 23:28:07 +0300

    Tetkikler aktivitesi bitmek üzere kişinin isminin getirilmesi için join ayarını yapıyorum

--- app/src/main/java/com/agaoglu/tez/tetkikler.java
+++ app/src/main/java/com/agaoglu/tez/tetkikler.java
@@ -3,9 +3,22 @@ package com.agaoglu.tez;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +26,9 @@ import android.view.ViewGroup;
  */
 public class tetkikler extends Fragment {
 
+    private DatabaseReference tetkikDB;
+    private DatabaseReference hastaDB;
+    private List<tetkik> tetkiklerList;
 
     public tetkikler() {
         // Required empty public constructor
@@ -23,7 +39,49 @@ public class tetkikler extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tetkikler, container, false);
+        View view = inflater.inflate(R.layout.fragment_tetkikler, container, false);
+        getActivity().setTitle("Tetkikler");
+
+        tetkiklerList = new ArrayList<>();
+        tetkikDB = FirebaseDatabase.getInstance().getReference("tetkikler");
+        hastaDB = FirebaseDatabase.getInstance().getReference("hastalar");
+
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.tetkiklerlist);
+        final SwipeRefreshLayout swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeRefreshtetkik);
+        final tetkikadapter adapter = new tetkikadapter(tetkiklerList, getActivity());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        tetkikDB.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot != null && dataSnapshot.getValue() != null) {
+                    tetkik tetkik = dataSnapshot.getValue(com.agaoglu.tez.tetkik.class);
+                    adapter.insert(0,tetkik);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        return view;
     }
 
 }
